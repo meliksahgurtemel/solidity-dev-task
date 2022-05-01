21c87a8ac298fa1276284f52fd169166326e2834
Meliksah Gurtemel <92724453+meliksahgurtemel@users.noreply.github.com>
2022-05-01 20:28:33 +0300

    Update aUSTVault.sol

--- contracts/src/integrations/aUSTVault.sol
+++ contracts/src/integrations/aUSTVault.sol
@@ -2,9 +2,14 @@
 pragma solidity 0.8.10;
 
 import "src/Vault.sol";
-
+import {IxAnchorBridge} from "src/interfaces/IxAnchorBridge.sol";
+import {AggregatorV3Interface} from "src/interfaces/AggregatorV3Interface.sol";
 
 contract aUSTVault is Vault {
+
+    IERC20 public aUST;
+    IxAnchorBridge public xAnchorBridge;
+    AggregatorV3Interface public priceFeed;
 
     function initialize(
         address _underlying,
@@ -13,7 +18,10 @@ contract aUSTVault is Vault {
         uint256 _adminFee,
         uint256 _callerFee,
         uint256 _maxReinvestStale,
-        address _WAVAX
+        address _WAVAX,
+        address _xAnchorBridge,
+        address _aUST,
+        address _priceFeed
     ) public {
         initialize(_underlying,
                     _name,
@@ -23,7 +31,60 @@ contract aUSTVault is Vault {
                     _maxReinvestStale,
                     _WAVAX
                     );
-        // TODO: Change this initializer as necessary
+        aUST = IERC20(_aUST);
+        priceFeed = AggregatorV3Interface(_priceFeed);
+        xAnchorBridge = IxAnchorBridge(_xAnchorBridge);
+        underlying.approve(address(xAnchorBridge), MAX_INT);
     }
-    // TODO: Override functions and add helpers as necessary
+
+    // Returns the latest price of aUST in UST
+    function getLatestPrice() public view returns (uint256) {
+        (
+            /*uint80 roundID*/,
+            int256 price,
+            /*uint256  startedAt*/,
+            /*uint256  timeStamp*/,
+            /*uint80 answeredInRound*/
+        ) = priceFeed.latestRoundData();
+        return uint256(price);
+    }
+
+    function receiptPerUnderlying() public override view returns (uint256) {
+        if (totalSupply == 0) {
+            return 10 ** (18 + 18 - underlyingDecimal);
+        }
+        uint256 _USTAmt = (aUST.balanceOf(address(this)) * getLatestPrice()) / 1e18;
+        return (1e18 * totalSupply) / _USTAmt;
+    }
+
+    function underlyingPerReceipt() public override view returns (uint256) {
+        if (totalSupply == 0) {
+            return 10 ** underlyingDecimal;
+        }
+        uint256 _USTAmt = (aUST.balanceOf(address(this)) * getLatestPrice()) / 1e18;
+        return (1e18 * _USTAmt) / totalSupply;
+    }
+
+    function totalHoldings() public view override returns (uint256) {
+        uint256 _USTAmt = (aUST.balanceOf(address(this)) * getLatestPrice()) / 1e18;
+        return _USTAmt;
+    }
+
+    function _pullRewards() internal override {
+        xAnchorBridge.claimRewards();
+    }
+
+    function _triggerDepositAction(uint256 _amt) internal override {
+        xAnchorBridge.depositStable(address(underlying), _amt);
+    }
+
+    function _triggerWithdrawAction(uint256 amtToReturn) internal override {
+        xAnchorBridge.redeemStable(address(aUST), amtToReturn);
+    }
+
+    // Emergency withdraw in case of previously failed operations
+    // Notice that this address is the Terra address of the token
+    function emergencyWithdraw(string calldata token) public {
+        xAnchorBridge.withdrawAsset(token);
+    }
 }
